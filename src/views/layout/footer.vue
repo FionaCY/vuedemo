dbea21e0f8f9c91537648acf517c212f186b4bcb
chenying <chenying@momenta.ai>
2019-01-15 22:53:16 +0800

    web

---
+++ src/views/layout/footer.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="layui-footer footer footer-index">
+    <div class="layui-main">
+        <p>2019 © <a>little-Blue</a> </p>
+        <p>
+            <a target="_blank">@成都信息工程大学</a>
+        </p>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
